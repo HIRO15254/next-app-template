6b7e23729923a60bc4feeb7ba51799294bb6a87c
HIRO15254 <hi089697@gmail.com>
2024-02-21 14:21:16 +0900

    ビルドエラー再修正

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,8 @@
 'use client';
 
-import {createTheme} from '@mantine/core';
+import {createTheme, MantineTheme} from '@mantine/core';
+import {PartialObjectDeep} from 'type-fest/source/partial-deep';
 
-export const theme = createTheme({
+export const theme: PartialObjectDeep<MantineTheme, {}> = createTheme({
   /* Put your mantine theme override here */
 });
