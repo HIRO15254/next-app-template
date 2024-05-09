e14c4b5848711e3df72f5bb200414b677dd093dd
HIRO15254 <hi089697@gmail.com>
2024-05-10 01:42:50 +0900

    fix typeerror

--- src/util/dummySession.ts
+++ src/util/dummySession.ts
@@ -7,5 +7,6 @@ export const dummySession: Session = {
     email: 'dummy-email@dummy.com',
     image: 'https://picsum.photos/240',
   },
+  token: 'dummy_token',
   expires: '2099-03-25T12:00:00.000',
 };
